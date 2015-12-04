db9b3d85799dc7b285959b25ef8ac065c6063bc6
Tim <timkeulen@gmail.com>
2015-12-04 09:03:26 +0100

    bug needs to be fixed

---
+++ Lisa.BulderMobile/Lisa.BulderMobile/Models/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lisa.BulderMobile
+{
+	public class Message
+	{
+		public string Text { get; set; }
+		public string Author { get; set; }
+		public string PartitionKey { get; set; }
+		public string RowKey { get; set; }
+		public DateTime TimeStamp { get; set; }
+		public string ETag { get; set; }
+	}
+} 
+
---
+++ Lisa.BulderMobile/Lisa.BulderMobile/Test.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using Xamarin.Forms;
+using System.IO;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace Lisa.BulderMobile
+{
+	public partial class Test : ContentPage
+	{
+		public Test ()
+		{
+			InitializeComponent ();
+		}
+
+		private async void GetMessages(object sender, EventArgs e)
+		{
+			using(var client = new HttpClient())
+			{
+				client.BaseAddress = new Uri("http://10.10.35.49:13693/");
+
+				var result = await client.GetAsync("/messages/AAA/");
+
+				if(result.StatusCode == HttpStatusCode.OK)
+				{
+					var json = await result.Content.ReadAsStringAsync();
+
+					var messages = JsonConvert.DeserializeObject<IEnumerable<Message>>(json);
+
+					foreach(var message in messages)
+					{
+						TeacherPicker.Items.Add(message.Text);
+					}
+				}
+			}
+		}
+	}
+}
+
